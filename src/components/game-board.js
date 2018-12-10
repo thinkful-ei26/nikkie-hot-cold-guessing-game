f5562b632f618ffe875ec9e5abf188fc7573bdd1
Nikkie Mashian <nikkiemashian@gmail.com>
2018-12-10 13:24:14 -0800

    static components finished

--- src/components/game-board.js
+++ src/components/game-board.js
@@ -8,15 +8,15 @@ import './game-board.css';
 
 export default function GameBoard() {
   const message = "Make A Guess!";
-
-  const amount = 0; 
+  const amount = 3; 
+  const guesses = [81, 25, 300];
 
     return (
       <div className="game-board">
         <HotOrCold message={message}/> 
         <GuessForm /> 
         <NumberOfGuesses amount = {amount} /> 
-        <ListOfGuesses />
+        <ListOfGuesses guesses = {guesses} />
       </div>
       );
     }
--- src/components/list-of-guesses.js
+++ src/components/list-of-guesses.js
@@ -4,9 +4,16 @@ import './list-of-guesses.css';
 
 export default function ListOfGuesses(props) {
     
+    const listElements = props.guesses.map((guess, index)=>(
+      // Why do I need to say props.guesses.map? Could I have done it differently?
+      <li key={index}>
+          {guess}
+        </li>
+    ));
+
     return (
       <ul className = "list-of-guesses">
-
+        {listElements}
       </ul>
       );
     }
